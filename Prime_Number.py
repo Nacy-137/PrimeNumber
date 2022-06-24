fcd5f4260abc93361953e2014e453883abd8476f
Nacy <94168841+Nacy-137@users.noreply.github.com>
2022-06-24 16:51:37 +0900

    Add files via upload

---
+++ Prime_Number.py
@@ -0,0 +1,28 @@
+"""
+入力した整数が素数であるかを判別
+"""
+
+def Divisor(n):
+    i = 1
+    _n = n + 1
+    divisor = []
+    
+    for i in range(1,_n):
+        if(len(divisor) >= 2):
+            break
+        
+        r = n % i
+        if(r == 0):
+            divisor.append(i)
+       
+    Judge(divisor)
+    
+def Judge(list):        
+    if(list[0] == 1 and list[1] == n):
+        print(n,"は素数です")
+    else:
+        print(n,"は素数ではありません")
+        
+
+n = int(input("整数を入力："))
+Divisor(n)
